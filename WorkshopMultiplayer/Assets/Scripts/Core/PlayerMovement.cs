54d4ef22ccf0059e8e5d339873b3cd4a892b1311
Wolverlone <gamesmews@hotmail.com>
2024-01-31 02:44:16 +0700

    Workshop 4 Step 1

--- WorkshopMultiplayer/Assets/Scripts/Core/PlayerMovement.cs
+++ WorkshopMultiplayer/Assets/Scripts/Core/PlayerMovement.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private InputReader inputReader;
@@ -12,15 +13,37 @@ public class PlayerMovement : MonoBehaviour
     [Header("Settings")]
     [SerializeField] private float movementSpeed = 4f;
     [SerializeField] private float turningRate = 30f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    private Vector2 previousMovementInput;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) { return; }
+        inputReader.MoveEvent += HandleMove;
+    }
+    public override void OnNetworkDespawn()
+    {
+        if(!IsOwner) { return; }
+        inputReader.MoveEvent -= HandleMove;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(!IsOwner) { return ; }
+
+        float zRotation = previousMovementInput.x * -turningRate * Time.deltaTime;
+        bodyTransform.Rotate(0f,0f,zRotation);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!IsOwner) { return ; }
+
+        rb.velocity = (Vector2)bodyTransform.up * previousMovementInput.y * movementSpeed;
+    }
+
+    private void HandleMove(Vector2 movementInput)
+    {
+        previousMovementInput = movementInput;
     }
 }
