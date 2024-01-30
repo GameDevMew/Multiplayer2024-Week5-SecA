1ca7a98afad0fa46143ea8849a4610634179492a
Wolverlone <gamesmews@hotmail.com>
2024-01-31 02:11:16 +0700

    First Step of Week 4

---
+++ WorkshopMultiplayer/Assets/Scripts/Core/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Transform bodyTransform;
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Settings")]
+    [SerializeField] private float movementSpeed = 4f;
+    [SerializeField] private float turningRate = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
